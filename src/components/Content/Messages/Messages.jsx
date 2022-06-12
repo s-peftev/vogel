72fd95f8a22de95c77f875c242508ef4a570f270
s-peftev <s.peftev@gmail.com>
2022-06-12 11:27:05 +0300

    map components added

--- src/components/Content/Messages/Messages.jsx
+++ src/components/Content/Messages/Messages.jsx
@@ -5,44 +5,50 @@ import Message from './Message/Message.jsx';
 
 class Messages extends React.Component {
   render() {
-    const dialogEmmaProps = {
-      id: 1,
-      photo: 'https://upload.wikimedia.org/wikipedia/commons/7/7f/Emma_Watson_2013.jpg',
-      name: 'Emma Watson',
-    };
-
-    const dialogCarloProps = {
-      id: 2,
-      photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Carlo_Ancelotti_2016_%28cropped%29.jpg/640px-Carlo_Ancelotti_2016_%28cropped%29.jpg',
-      name: 'Carlo Ancelotti',
-    };
-
+    const dialogs = [
+      {
+        id: 1,
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/7/7f/Emma_Watson_2013.jpg',
+        name: 'Emma Watson',
+      },
+      {
+        id: 2,
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Carlo_Ancelotti_2016_%28cropped%29.jpg/640px-Carlo_Ancelotti_2016_%28cropped%29.jpg',
+        name: 'Carlo Ancelotti',
+      }
+    ];
     const messages = [
-      'Hello',
-      'What are u doing next Sat?',
-      '=)'
-    ]
+      {
+        id: 1,
+        text: 'Hello',
+      },
+      {
+        id: 2,
+        text: 'What are u doing next Sat?',
+      },
+      {
+        id: 3,
+        text: '=)',
+      },
+    ];
+    const dialogElements = dialogs.map((dialog) => {
+      return <div key={dialog.id} className={s.dialog}>
+        <Dialog key={dialog.id} {...dialog} />
+      </div>
+    })
+    const messageElements = messages.map((message) => {
+      return <div key={message.id} className={s.message}>
+        <Message key={message.id} {...message} />
+      </div>
+    })
 
     return (
       <div className={s.messages}>
         <div className={s.dialogs}>
-          <div className={s.dialog}>
-            <Dialog  {...dialogEmmaProps} />
-          </div>
-          <div className={s.dialog}>
-            <Dialog {...dialogCarloProps} />
-          </div>
+          {dialogElements}
         </div>
         <div className={s.messages_container}>
-          <div className={s.message}>
-            <Message text={messages[0]} />
-          </div>
-          <div className={s.message}>
-            <Message text={messages[1]} />
-          </div>
-          <div className={s.message}>
-            <Message text={messages[2]} />
-          </div>
+          {messageElements}
         </div>
       </div>
     );
--- src/components/Content/Profile/Profile.jsx
+++ src/components/Content/Profile/Profile.jsx
@@ -16,41 +16,12 @@ class Profile extends React.Component {
         about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ipsa quam tempore unde totam ipsum quis aspernatur et reprehenderit ad iure aliquam dicta, officia, praesentium illum cum nisi! Excepturi, a!',
         photo: 'https://images.squarespace-cdn.com/content/v1/5b6a682125bf02f85e285689/1546478840771-HG0TE1RQUIZGUHHY2TKW/edited.jpg?format=1000w',
       },
-      posts: [
-        {
-          name: 'Gustav Header',
-          login: '@gustav-header',
-          photo: 'https://images.squarespace-cdn.com/content/v1/5b6a682125bf02f85e285689/1546478840771-HG0TE1RQUIZGUHHY2TKW/edited.jpg?format=1000w',
-          date: '30 May 2022',
-          text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore quo culpa repellat.',
-          likes_count: '42',
-          comments_count: '2',
-        },
-        {
-          name: 'Emma Watson',
-          login: '@emma-watson',
-          photo: 'https://upload.wikimedia.org/wikipedia/commons/7/7f/Emma_Watson_2013.jpg',
-          date: '5 May 2022',
-          text: 'Hello! Glad to see you here!',
-          likes_count: '31',
-          comments_count: '0',
-        },
-        {
-          name: 'Carlo Ancelotti',
-          login: '@mrancelotti',
-          photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Carlo_Ancelotti_2016_%28cropped%29.jpg/640px-Carlo_Ancelotti_2016_%28cropped%29.jpg',
-          date: '26 April 2022',
-          text: 'Hello there! See my Instagram photos and videos! (@mrancelotti)',
-          likes_count: '3',
-          comments_count: '0',
-        },
-      ]
     }
 
     return (
       <div className={s.profile}>
         <UserCard parrentClass={s.user_card} {...user} />
-        <Posts parrentClass={s.posts} {...user}/>
+        <Posts parrentClass={s.posts} />
       </div>
     );
   }
